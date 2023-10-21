ad36e6912779c5a597c72d8bacb7c2e8874adc5a
rokberri <rokberri@gmail.com>
2023-10-21 17:12:42 +0300

    comments added

--- hash_function.py
+++ hash_function.py
@@ -1,7 +1,7 @@
 from feistel_net import encode,decode,encryption,decryption
 from binary_operations import *
 
-
+# Функция деления всей битовой строки на блоки заданного размера, просто для удобства итераций
 def split_into_blocks(bins:str, size=64)->list[str]:
     list_of_blocks = list()
     tmp = ''
@@ -12,25 +12,31 @@ def split_into_blocks(bins:str, size=64)->list[str]:
             tmp = ''
     return list_of_blocks 
 
+# Хеш-функция
 def hash_function(bins:str,f, use_fin_func=False)->str:
+    # Начальный вектор инициализации(считай секретный ключ)
     IV = '1101110110111010110001001101001000000010110110101000111010100010'
     hash_code = ''
     blocks_of_bins = split_into_blocks(bins)
+    # Для каждого блока по 64 бита
     for block in blocks_of_bins:
+        # если это первый блок, ксорим его с вектором инициализации по схеме
         if len(hash_code) == 0:
             hash_code = xor(block,IV)
             hash_code = encryption(hash_code, IV, f)
             hash_code = xor(hash_code, block)
         else:
+            # если не первый - то ксорим с предыдущим результатом, опять же по схеме
             prev_hash = hash_code
             hash_code = xor(block,prev_hash)
             hash_code = encryption(hash_code, prev_hash, f)
             hash_code = xor(hash_code, block)
+    # Ну и если нам нужно усечь значение хеш-функции усекаем или выводим как есть
     if use_fin_func:
         return finalization_func(hash_code)
     else:
         return hash_code
 
-
+# Функция финализации(по факту просто берет какое-то количество бит хеша вместо полгого хеша)
 def finalization_func(hash_code:str)->str:
     return hash_code[56:64]
--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from binary_operations import *
 import random
 from hash_function import hash_function
 
+# В краце что я поменял в твоем коде. 
+# 1) Раскидал функционал по файликам(вся шифрация в один, битовые операции в друго и тд)
+# 2) Изменил сигнатуру сети Фейстеля. На вход теперь подается битовая последовательность
+#     + функиця для шифрации (f) + булевая константа, которая определяет использовать ли функцию финализации
+# 3) Вынес повторяющийся код в отдельные функции (например то, где ты файл читаешь)
 
+# ------------------------Блок твоего кода, просто слегка измененный---------------------------------------
 def read_data_from_file(filepath='text.txt')->str:
     file = open("text.txt", "r", encoding='utf-8')
     file_text = file.read()
@@ -23,7 +29,9 @@ def f(m0, m1, round_key):
 def random_key(length):
    letters = ['0', '1']
    return ''.join(random.choice(letters) for i in range(length))
+# ---------------------------------------------------------------------------------------------------------
 
+# Просто переписанный в функцию код из твоего 1 таска, проверяет работоспособность сети Фейстеля
 def test_feistel_net():
     file_text = read_data_from_file()
     print(file_text)
@@ -38,76 +46,90 @@ def test_feistel_net():
     print('Расшифрованный текст', dec)
     print(decode(dec))
 
+# Функция для теста хеш-функции, сама реализация в файлике я hash_function
+# По факту это первое задание из 3 в 3 таске
 def test_hash_func():
     file_text = read_data_from_file()
-    # print(file_text)
     bin_text = get_binary_data(file_text)
-
+# По логике тут все так же, как в шифровании сетью Фейстеля, просто используем в одном месте другую функцию
+# Ну и результат хеш-функции нельзя преобразовать обратно, так что декода нет
     print('Изначальный текст   ', bin_text)
     secret_key = random_key(64)
     en = hash_function(bin_text, f,USE_FINALISATION_FUNCTION)
     print('Зашифрованный текст ', en)
 
-def generate_text(length:int)->str:
-    ALPH =list("""0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя""")
-    text = ''
-    for i in range(length):
-        text += random.choice(ALPH)
-    return text
-
-def searching():
-    pass
-
-
-def searching_for_collisions():
-    file_text = read_data_from_file()
-    bin_text = get_binary_data(file_text)
-    origin_hash = hash_function(bin_text,f, USE_FINALISATION_FUNCTION)
-    list_of_words = file_text.split()
-    print(origin_hash)
-    for i in range(len(list_of_words)):
-        for j in range(257):
-            new_word = chr(j)
-            # list_of_words[i] = new_word
-            list_of_sim = list(file_text)
-            if not list_of_sim[i] == new_word:
-                list_of_sim[i] = new_word
-            else: 
-                break
-            bin_text = get_binary_data(''.join(list_of_sim))
-            new_hash = hash_function(bin_text,f, USE_FINALISATION_FUNCTION)
-            if origin_hash == new_hash:
-                print(f"{new_word},{i}")
-                print(''.join(list_of_sim))
-
-
-USE_FINALISATION_FUNCTION = True
-
+# Второе задание. 
+# Функция для поиска коллизий(два различных текста, дающие одинаковый хеш)
 def find_collisions():
+    # Задаем словарь для храниния пар хеш-открытый текст
     hash_dict = {}
     i = 0
+    # Бесконечный цикл
     while True:
-        text = str(i)  # Используем целое число в качестве открытого текста
+        # Используем целое число в качестве открытого текста
+        # Нужно для упрощения, потому что по факту любая битовая последовательность 
+        # может быть преобразована в число
+        text = str(i)  
+        # Тут получаем битовое представление числа
         bin_text = get_binary_data(text)
+        # Получаем его хеш
         hash_code = hash_function(bin_text,f,USE_FINALISATION_FUNCTION)
-        
+        # Если хеш-код уже присутствует в словаре, значит мы нашли пару текстов с одинаковым хеш-кодом
         if hash_code in hash_dict:
-            # Если хеш-код уже присутствует в словаре, значит мы нашли пару текстов с одинаковым хеш-кодом
             print(bin_text)
             return (text, hash_dict[hash_code])
         
         # Добавляем текущий хеш-код в словарь для дальнейшего сравнения
         hash_dict[hash_code] = text
-        
+        # Увеличиваем число
         i += 1
+
+
+# Третье задание
+# Функция для поиска текста, который бы имел отличие не более чем в одном слове
+# По факту кстати это задания на поиск коллизий разного рода
+# Разница в количестве различающихся знаков, если интересно, спроси, это текстом долго писать)
+def searching_for_collisions():
+    # Читаем все из файла, переводим в биты и находим оригинальный хеш
+    file_text = read_data_from_file()
+    bin_text = get_binary_data(file_text)
+    origin_hash = hash_function(bin_text,f, USE_FINALISATION_FUNCTION)
+    # Делим весь текст на слова
+    list_of_words = file_text.split()
+    print(origin_hash)
+    # Пробуем подставлять все символы на место каждого символа
+    # (каждую итерацию меняется один символ) 
+    # для конкретно этой функции достаточно перебрать 1 символ, там велик шанс найти коллизию 
+    # в целом же нужно перебирать гораздо больше и менять слова на несколько символов
+    for i in range(len(file_text)):
+        for j in range(257):
+            # Получаем новый символ, который подставлять будем
+            new_word = chr(j)
+            list_of_sim = list(file_text)
+            # чтоб не было такого, что мы меняем символ на такой же и получаем тот же хеш
+            if not list_of_sim[i] == new_word:
+                list_of_sim[i] = new_word
+            else: 
+                break
+            # Собираем строчку обратно после изменения символа
+            # И находим ее хеш
+            bin_text = get_binary_data(''.join(list_of_sim))
+            new_hash = hash_function(bin_text,f, USE_FINALISATION_FUNCTION)
+            # Если новая строчка имеет тот же хеш что и оригинальная выводим символ, позицию и новый текст
+            if origin_hash == new_hash:
+                print(f"{new_word},{i}")
+                print(''.join(list_of_sim))
+
+# константа для удобства просто
+USE_FINALISATION_FUNCTION = True
 
 if __name__ == '__main__':
     # test_feistel_net()
     
-    test_hash_func()
+    # test_hash_func()
    
     # collisions = find_collisions()
     # print("Первый текст:", collisions[0],'HASH:',hash_function(get_binary_data(collisions[0]),f,USE_FINALISATION_FUNCTION))
     # print("Второй текст:", collisions[1],'HASH:',hash_function(get_binary_data(collisions[1]),f,USE_FINALISATION_FUNCTION))
    
-    # searching_for_collisions()
+    searching_for_collisions()
